24f2a1d856a9cfaf92d10d48a42318694f0e6490
Leticia Kawamoto <leticiac.0401@gmail.com>
2023-06-30 11:26:27 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import * as S from './styles';
 import Header from '../../components/Header';
 
 const Home = () => {
+  const [restaurantData, setRestaurantData] = useState([]);
+
+  useEffect(() => {
+    const fetchRestaurantData = async () => {
+      try {
+        const response = await axios.get(
+          'https://raw.githubusercontent.com/evercodeinc/meatless-api/main/restaurant_list.json'
+        );
+        setRestaurantData(response.data);
+      } catch (error) {
+        console.error('Error fetching restaurant data:', error);
+      }
+    };
+
+    fetchRestaurantData();
+  }, []);
+
   return (
     <S.Container data-testid="meatless-pageHome">
-      <Header adress="R. Clodomiro Amazonas, 22" />
+      <Header address="R. Clodomiro Amazonas, 22" />
       <S.Main>
         <h2>Restaurantes (todos)</h2>
         <ul data-testid="meatless-restaurantList">
-          {/* show your restaurants cards here! */}
+          {restaurantData.map((restaurant) => (
+            <li key={restaurant.id} data-testid="meatless-restaurantCard">
+              {restaurant.name}
+            </li>
+          ))}
         </ul>
       </S.Main>
     </S.Container>
-  )
-}
+  );
+};
 
 export default Home;
